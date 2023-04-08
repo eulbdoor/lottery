15953818a5e3c0694b6dd76aa53322b676f1b0f9
eulbdoor <eulbdoor@gmail.com>
2023-04-08 16:02:03 +0100

    Add test - start lottery

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -5,12 +5,5 @@ from brownie import Lottery, accounts
 def deployer():
       return accounts.load("deployment_account")
 
-@pytest.fixture(scope="module")
-def token(Lottery, deployer):
-        deployer.deploy(Lottery, 0.19, 10)
-
-def test_account_balance(token):
-    balance = accounts[0].balance()
-    accounts[0].transfer(accounts[1], "10 ether", gas_price=0)
-
-    assert balance - "10 ether" == accounts[0].balance()
+def test_deploy(Lottery, deployer):
+      _ = deployer.deploy(Lottery, 0.05, 10)
