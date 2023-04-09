8ded08eabfbc2f61c87508e0ecbfd94ddbeb048b
eulbdoor <eulbdoor@gmail.com>
2023-04-09 01:06:55 +0100

    Add tests - start lottery

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -3,7 +3,7 @@ from brownie import Lottery, accounts
 
 @pytest.fixture(scope="module")
 def deployer():
-      return accounts.load("deployment_account")
+    return accounts.load("deployment_account")
 
 def test_deploy(Lottery, deployer):
-      _= deployer.deploy(Lottery, 0.19, 10)
+    _= deployer.deploy(Lottery, 0.05, 10)
--- tests/test_start_lottery.py
+++ tests/test_start_lottery.py
@@ -1,13 +1,29 @@
-import pytest 
-from brownie import Lottery, accounts
+import brownie, pytest 
+from brownie import Lottery, accounts 
 
 @pytest.fixture(scope="module")
 def deployer():
-      return accounts.load("deployment_account")
+    return accounts.load("deployment_account")
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def token(Lottery, deployer):
-      return deployer.deploy(Lottery, 0.05, 10)
+    return deployer.deploy(Lottery, 0.05, 10)
 
-def test_start_lottery_with_owner(token, deployer):
-      token.start_lottery({'from': deployer})
+@pytest.fixture(autouse=True)
+def isolation(fn_isolation):
+    pass
+
+def test_start_lottery(token, deployer):
+    token.start_lottery({'from': deployer})
+
+def test_start_lottery_twice(token, deployer):
+    token.start_lottery({'from': deployer})
+    with brownie.reverts(""):
+        token.start_lottery({'from': deployer})
+
+def test_start_lottery_not_authorized(token):
+    with brownie.reverts("Ownable: caller is not the owner"):
+        token.start_lottery({'from': accounts[0]})
+
+
+# Validar com o erro.
